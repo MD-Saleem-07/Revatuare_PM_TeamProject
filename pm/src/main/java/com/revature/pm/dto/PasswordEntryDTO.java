cf213009358dc0e73e219cc76da8fd03ab41c34d
MD-Saleem-07 <mdsaleemagasar@gmail.com>
2026-02-23 22:44:14 +0530

    Feat:Implemented PasswordEntry and DTO layers and PasswordEntryMapper class

---
+++ pm/src/main/java/com/revature/pm/dto/PasswordEntryDTO.java
@@ -0,0 +1,102 @@
+package com.revature.pm.dto;
+
+import java.time.LocalDateTime;
+
+public class PasswordEntryDTO {
+
+    private Long id;
+    private String accountName;
+    private String websiteUrl;
+    private String loginUsername;
+    private String password;   // decrypted version for display
+    private String category;
+    private String notes;
+    private boolean favorite;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public PasswordEntryDTO() {
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public String getWebsiteUrl() {
+		return websiteUrl;
+	}
+
+	public void setWebsiteUrl(String websiteUrl) {
+		this.websiteUrl = websiteUrl;
+	}
+
+	public String getLoginUsername() {
+		return loginUsername;
+	}
+
+	public void setLoginUsername(String loginUsername) {
+		this.loginUsername = loginUsername;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public boolean isFavorite() {
+		return favorite;
+	}
+
+	public void setFavorite(boolean favorite) {
+		this.favorite = favorite;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+   
+}
