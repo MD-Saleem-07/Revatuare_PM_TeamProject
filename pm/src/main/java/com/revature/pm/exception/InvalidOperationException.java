3dfb5b1bbee7308f85cac239858b7476a8f49892
saikumar28072003 <vayalasaikumar5250@gmail.com>
2026-02-24 10:03:18 +0530

    feat:handled own exceptions added

---
+++ pm/src/main/java/com/revature/pm/exception/InvalidOperationException.java
@@ -0,0 +1,8 @@
+package com.revature.pm.exception;
+
+public class InvalidOperationException extends RuntimeException {
+
+    public InvalidOperationException(String message) {
+        super(message);
+    }
+}
---
+++ pm/src/main/java/com/revature/pm/exception/ResourceAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.revature.pm.exception;
+
+public class ResourceAlreadyExistsException extends RuntimeException {
+
+    public ResourceAlreadyExistsException(String message) {
+        super(message);
+    }
+}
